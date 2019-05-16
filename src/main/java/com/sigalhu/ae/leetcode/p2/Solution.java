9fee0bfb8a2be4f2f617bf627ff294cc5bb08a73
xujun <xujun@gegejia.com>
2019-05-17 02:10:33 +0800

    update p2 solution

--- src/main/java/com/sigalhu/ae/leetcode/p2/Solution.java
+++ src/main/java/com/sigalhu/ae/leetcode/p2/Solution.java
@@ -8,33 +8,17 @@ public class Solution {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int val = l1.val + l2.val;
-        ListNode head = new ListNode(val % 10);
-        ListNode tail = head;
+        ListNode head = new ListNode(val % 10), tail = head;
         val /= 10;
         l1 = l1.next;
         l2 = l2.next;
-        while (l1 != null && l2 != null) {
-            val += l1.val + l2.val;
+        while (l1 != null || l2 != null) {
+            val += (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
             tail.next = new ListNode(val % 10);
             val /= 10;
             tail = tail.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        if (l1 != null) {
-            do {
-                val += l1.val;
-                tail.next = new ListNode(val % 10);
-                val /= 10;
-                tail = tail.next;
-            } while ((l1 = l1.next) != null);
-        } else if (l2 != null) {
-            do {
-                val += l2.val;
-                tail.next = new ListNode(val % 10);
-                val /= 10;
-                tail = tail.next;
-            } while ((l2 = l2.next) != null);
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
         if (val > 0) {
             tail.next = new ListNode(val);
